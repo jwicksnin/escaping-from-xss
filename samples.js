51322b16a8e93642706005a2cb426f3ba45e7607
jwicksnin <jwicksnin@users.noreply.github.com>
2014-02-24 21:51:02 -0800

    Update samples.js

--- samples.js
+++ samples.js
@@ -1,3 +1,5 @@
+// This approach is more of a catch-all and allows you to dictate which characters and how it is escaped.
+
 function escapeHTML (unsafe_str) {
     	return unsafe_str
 	      .replace(/&/g, '&amp;')
@@ -5,9 +7,11 @@ function escapeHTML (unsafe_str) {
 	      .replace(/>/g, '&gt;')
 	      .replace(/\"/g, '&quot;')
 	      .replace('/', '&#x2F;')
-	      .replace(/\'/g, '&#x27;'); //
+	      .replace(/\'/g, '&#x27;'); 
 }
 
+
+// This approach is not as effective, but better than nothing.
 function escapeHtml(str) {
     var div = document.createElement('div');
     div.appendChild(document.createTextNode(str));
