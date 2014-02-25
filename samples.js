69b8e207bd09e343f9a8acef38c8b464f89c6040
jwicksnin <jwicksnin@users.noreply.github.com>
2014-02-24 21:46:46 -0800

    Create samples.js

---
+++ samples.js
@@ -0,0 +1,15 @@
+function escapeHTML (unsafe_str) {
+    	return unsafe_str
+	      .replace(/&/g, '&amp;')
+	      .replace(/</g, '&lt;')
+	      .replace(/>/g, '&gt;')
+	      .replace(/\"/g, '&quot;')
+	      .replace(///g, '&#x2F;')
+	      .replace(/\'/g, '&#x27;'); //
+}
+
+function escapeHtml(str) {
+    var div = document.createElement('div');
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+}
